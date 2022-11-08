34d0f93d98acaa3a2e75dfe12be74d22a055b497
lucasgstumpf <lucasgstumpf@gmail.com>
2022-11-08 00:02:08 -0300

    Add gramatica

--- js/gramatica.js
+++ js/gramatica.js
@@ -1,24 +1,50 @@
-function addTable(primeiro,segundo){
-    var tb = document.getElementById("tbGramatica");
-    var qdtLinhas = tb.rows.length;
-    var linha = tb.insertRow(qdtLinhas);
 
-    var cellCodigo = linha.insertCell(0);
-    var cellPrimeiro = linha.insertCell(1);
-    var cellSegundo = linha.insertCell(2);
 
-    cellCodigo.innerHTML = qdtLinhas;
-    cellPrimeiro.innerHTML = primeiro;
-    cellSegundo.innerHTML = segundo;
-}
 
-const btn = document.getElementById("send");
+const btnTeste = document.querySelector("#sendTeste");
 
-btn.addEventListener("click", function (e) {
+btnTeste.addEventListener("click", function (e) {
     e.preventDefault();
-    const primeiro = document.getElementById("primeiro");
-    const segundo = document.getElementById("segundo");
 
-    addTable(primeiro.value,segundo.value)
+    var inputArea = document.getElementById("inputArea");
+    var inputAreaTxt = inputArea.value;
+    inputAreaTxt = inputAreaTxt.replace(/([A-Z])/g, ' $1');
+    inputAreaTxt = inputAreaTxt.split("\n");
+    console.log(inputAreaTxt);
+
+    var inputTeste = document.getElementById("inputTeste");
+    let token = inputTeste.value.split('');
     
+
+    // biblioteca de gramática
+    try {
+        var gramatica = new tinynlp.Grammar(inputAreaTxt);
+        inputArea.style.backgroundColor = "white"
+      } catch (error) {
+        console.error(error);
+        inputArea.style.backgroundColor = "red"
+      }
+
+    // definindo a raiz
+    const raiz = inputAreaTxt[0][1]
+
+    // contruindo o analizador
+    let saida = tinynlp.parse(
+        token,
+        gramatica,
+        raiz
+    );
+
+    // análise finalizadas
+    let estado = saida.getFinishedRoot(raiz);
+
+    if (estado) {
+        console.log("Entrou no if")
+        inputTeste.style.backgroundColor = "green";
+        return;
+    }
+    else {
+        inputTeste.style.backgroundColor = "red"
+    }
+
 });
