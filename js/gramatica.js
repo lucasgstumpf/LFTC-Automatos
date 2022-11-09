544e74b3c0ae8c4b263aef5e8856c76b0d05f1dc
lucasgstumpf <lucasgstumpf@gmail.com>
2022-11-08 21:29:43 -0300

    Tratamento de erros

--- js/gramatica.js
+++ js/gramatica.js
@@ -1,9 +1,6 @@
-
-
-
 const btnTeste = document.querySelector("#sendTeste");
 
-
+const regexTratamento = new RegExp("\s*[A-Z]->([a-z]|[0-9])*\s*([A-Z]{0,1})")
 //Função é chamada quando o botão é clicado
 btnTeste.addEventListener("click", function (e) {
     e.preventDefault();
@@ -11,16 +8,26 @@ btnTeste.addEventListener("click", function (e) {
     //Inputs e tratamentos de erros
     var inputArea = document.getElementById("inputArea");
     var inputAreaTxt = inputArea.value;
+
     inputAreaTxt = inputAreaTxt.replace(/([A-Z])/g, ' $1');
     inputAreaTxt = inputAreaTxt.split("\n");
     console.log(inputAreaTxt);
     var inputTeste = document.getElementById("inputTeste");
     let teste = inputTeste.value.split('');
 
-
+    for (let texto of inputAreaTxt){
+        console.log(regexTratamento.test(texto))
+        if (regexTratamento.test(texto)){
+            console.log("Ok")
+        }else{
+            inputArea.style.backgroundColor = "red"
+            return;
+        }
+    }
     //Função que valida a gramatica do input, o parametro é a gramatica em si em formato de array.
     try {
         var gramatica = new tinynlp.Grammar(inputAreaTxt);
+        
         inputArea.style.backgroundColor = "white"
     } catch (error) {
         console.error(error);
@@ -35,7 +42,7 @@ btnTeste.addEventListener("click", function (e) {
         gramatica,
         inicio
     );
-
+        
     let validacao = saida.getFinishedRoot(inicio);
 
     if (validacao) {
